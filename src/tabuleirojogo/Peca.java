fd6361f25d7c294f47b2b2c368a36798f3c0d812
MatheusInte <matheus.interaminense.guerra@gmail.com>
2023-04-03 20:14:43 -0300

    Inicio da implementação do Tabuleiro e Peca

---
+++ src/tabuleirojogo/Peca.java
@@ -0,0 +1,15 @@
+package tabuleirojogo;
+
+public class Peca {
+    protected Posicao posicao;
+    private Tabuleiro tabuleiro;
+
+    public Peca(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+        posicao = null;
+    }
+
+    protected Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+}
