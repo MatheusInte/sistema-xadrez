45a6fcef2afc32643419009b05feff67d2463b37
MatheusInte <matheus.interaminense.guerra@gmail.com>
2023-04-03 20:42:32 -0300

    Package xadrez e impressão de tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,11 @@ package application;
 
 import tabuleirojogo.Posicao;
 import tabuleirojogo.Tabuleiro;
+import xadrez.PartidaDeXadrez;
 
 public class Program {
     public static void main(String[] args) {
-        Tabuleiro tabuleiro = new Tabuleiro(8,8);
+        PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
+        UI.printTabuleiro(partidaDeXadrez.getPecas());
     }
 }
